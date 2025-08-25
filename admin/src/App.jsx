d2c36fdf3cc7c9062f0695007124e75220b8bd66
Aakrit-Adhikari <aakritadhikari2002.com>
2025-08-25 09:31:36 +0545

    added addproducts in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/admin'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Admin/>
     </div>
   )
 }
--- admin/src/Pages/Admin/admin.jsx
+++ admin/src/Pages/Admin/admin.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./admin.css"
-const admin = () => {
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import AddProduct from '../../Components/AddProduct/AddProduct'
+import ListProduct from '../../Components/ListProdcut/ListProduct'
+
+const Admin = () => {
   return (
     <div className='admin'>
-      
+      <Sidebar/>
+     <Routes>
+      <Route path='/addproduct' element={<AddProduct/>}/>
+      <Route path='/listproduct' element={<ListProduct/>}/>
+
+     </Routes>
     </div>
   )
 }
 
-export default admin
+export default Admin
